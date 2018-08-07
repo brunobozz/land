cd724221e820898bf17cab5dc10bc506c7604875
Bruno Tourn Bozzetti <bruno.bozzetti@srmasset.com>
2018-08-07 15:16:44 -0300

    parallax

--- src/app/comp/parallax-area/parallax-area.component.ts
+++ src/app/comp/parallax-area/parallax-area.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterContentInit } from '@angular/core';
+import { Component, OnInit, AfterContentInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-parallax-area',
@@ -9,7 +9,28 @@ export class ParallaxAreaComponent implements OnInit {
 
   constructor() { }
 
+
+
   ngOnInit() {
+
+    $('div.bg-parallax').each(function () {
+      var $obj = $(this);
+
+      $(window).scroll(function () {
+        var yPos = -($(window).scrollTop() / $obj.data('speed'));
+
+        var bgpos = '50% ' + yPos + 'px';
+
+        $obj.css('background-position', bgpos);
+
+      });
+    });
+
+  }
+
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    console.log('coco');
   }
 
 }
